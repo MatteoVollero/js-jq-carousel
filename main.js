5290828aa51056df64843d9577833c4065f3cb6e
MatteoVollero <m.vollero120796@gmail.com>
2020-09-02 17:06:26 +0200

    commenti

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ $(document).ready(function(){
 
   $(".next").click(
     function(){
+      //prende la classe active presente sia nell'immagine che nei pallini
       var currentImg = $(".active");
+      //Rimuove la classe active che gli da visibiltà
       currentImg.removeClass("active");
 
+      //Controlla che non si trovi all'ultima immagine e nel caso torna alla prima
       if(currentImg.hasClass("last") == true){
         var nextImg = $(".first");
         console.log("ultima foto");
       }else{
+        //Passa all'immagine successiva
         var nextImg = currentImg.next();
       }
+      //Aggiunge la classe active
       nextImg.addClass("active");
     });
 
@@ -19,11 +24,14 @@ $(document).ready(function(){
         var currentImg = $(".active");
         currentImg.removeClass("active");
 
+        //Controlla che non si trovi alla prima immagine e nel caso torna all'ultima
         if(currentImg.hasClass("first") == true){
           var prevImg = $(".last");
         }else{
+          //Passa all'immagine precedente
           var prevImg = currentImg.prev();
         }
+        //Aggiunge la classe active
         prevImg.addClass("active")
       });
 
